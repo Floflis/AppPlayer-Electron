42e32c4b0b714d9266c9f825e12493616249fbb3
Quentin Goinaud <armaldio@gmail.com>
2020-09-10 20:44:35 +0200

    update to latest Electron
    remove unused jquery references
    add support for native HTML export from construct
    better configuration handler for clearer code

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;app.commandLine.appendSwitch('--enable-experimental-web-platform-features');
-const fs = require("fs");
-const path = require("path");
+const { app, BrowserWindow } = require('electron');
+const { existsSync, readFileSync } = require('fs');
+const { join } = require('path');
+const handler = require('serve-handler');
 
 const isDev = require('electron-is-dev');
 
 let mainWindow;
 let config;
-let def = true;
 
 let args = process.argv;
-global.args = args;
 
-/*app.commandLine.appendSwitch('force-gpu-rasterization');
+/*
+app.commandLine.appendSwitch('force-gpu-rasterization');
+app.commandLine.appendSwitch('--enable-experimental-web-platform-features');
 app.commandLine.appendSwitch('ignore-gpu-blacklist');
 app.commandLine.appendSwitch('enable-es3-apis');
 app.commandLine.appendSwitch('enable-webgl-draft-extensions');
@@ -27,48 +26,72 @@ app.commandLine.appendSwitch('enable-html5-camera');
 app.commandLine.appendSwitch('allow-file-access-from-files');
 app.commandLine.appendSwitch('disable-software-rasterizer');*/
 
-if (fs.existsSync(path.join(__dirname, "config.json"))) {
-	config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), 'utf-8'));
-
-	console.log(config);
-
-	def = false;
+if (existsSync(join(__dirname, 'config.json'))) {
+	config = JSON.parse(readFileSync(join(__dirname, 'config.json'), 'utf-8'));
 } else {
-	console.log("No configuration file, back to default settings");
+	console.log('No configuration file, back to default settings');
+}
+
+const defaultConfig = {
+	window: {
+		width: 800,
+		height: 600,
+		frame: false,
+		fullscreen: false,
+		transparent: true,
+		alwaysOnTop: false
+	},
+	developer: {
+		show_dev_tools: false
+	}
+}
+
+const appConfig = {
+	...defaultConfig,
+	...config
 }
 
 function createWindow() {
-	console.log('preload', path.join(__dirname, './preload.js'))
+	console.log('preload', join(__dirname, './preload.js'))
 	mainWindow = new BrowserWindow({
 		webPreferences: {
 			plugins: true,
 			nodeIntegration: true,
-			preload: path.join(__dirname, 'preload.js')
+			// TODO replace by ipc calls
+			enableRemoteModule: true,
+			preload: join(__dirname, 'preload.js')
 		},
-		width: (def ? 800 : config.window.width),
-		height: (def ? 600 : config.window.height),
-		frame: (def ? false : config.window.frame),
-		fullscreen: (def ? false : config.window.fullscreen),
-		transparent: (def ? true : config.window.transparent),
-		alwaysOnTop: (def ? false : config.window.alwaysOnTop),
+		width: appConfig.window.width,
+		height: appConfig.window.height,
+		frame: appConfig.window.frame,
+		fullscreen: appConfig.window.fullscreen,
+		transparent: appConfig.window.transparent,
+		alwaysOnTop: appConfig.window.alwaysOnTop,
 	});
 
 	if (process.argv[1]) {
 		console.log('process.argv[1]', process.argv[1])
-		// mainWindow.loadFile('./index-preview.html');
 		mainWindow.loadURL(process.argv[1]);
 	}
 	else {
-		console.log(`file://${__dirname}/www/index.html`)
-		mainWindow.loadURL(`file://${__dirname}/www/index.html`);
+
+		const config = {
+			public: `${__dirname}/www`,
+			directoryListing: false
+		};
+
+		require('http').createServer(function (request, response) {
+			return handler(request, response, config)
+		}).listen(8080);
+		mainWindow.loadURL(`http://localhost:8080`);
 	}
 
-	if ((def ? false : config.developer.show_dev_tools))
+	if ((appConfig.developer.show_dev_tools))
 		mainWindow.webContents.openDevTools();
 
 	if (isDev) {
 		console.log('Running in development');
-		console.log("cmd line arguments : ", args);
+		console.log('cmd line arguments : ', args);
 	} else {
 		console.log('Running in production');
 	}
